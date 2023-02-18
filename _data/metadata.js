f266de25c5e785b04e3283c5981c7a1fef038555
shawn ritchie <me@shawnritchie.com>
2023-02-17 21:52:26 -0600

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
+	"title": "shawn ritchie dot com",
+	"url": "https://www.shawnritchie.com/",
 	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
+	"description": "threading a path through the end times",
 	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
+		"name": "smr",
+		"email": "smr@hey.com",
+		"url": "https://www.shawnritchie.com/about_me"
 	}
 }
